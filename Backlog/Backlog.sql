5fdc157815b778cc888c0acab6749d31bd86a20a
Emilia Brzezinska <emilia.brzezinska@gmail.com>
2023-09-17 00:31:59 +0200

    task 5 in progress

--- Backlog/Backlog.sql
+++ Backlog/Backlog.sql
@@ -1,9 +1,7 @@
 USE Library_EmiliaB;
 
--- jako czlonek zespolu sprzedazy chce miec mozliwosc stworzenia nowego klienta.
--- powinien moc zapisac imie i nazwisko, adres i numer telefonu
--- z tego co rozumiem to poniewaz stworzylismy trzy tabele: employees, deloivery i sales
--- musimy teraz przy tworzneiu nowego klienta podac id pracownika sprzedazy, ktory
--- utowrzyl nowego klienta w bazie
+-- jako czlonek zespolu sprzedazy chce miec mozliwosc stworzenia nowego projektu.
+-- Powinienem być w stanie przechowywać nazwę projektu, wartość i listę technologii,
+-- z których będzie korzystał projekt
 
-INSERT INTO Clients  (name, adress, phone, semp_id ) VALUES ('Joanna Allen', 'Alben 123', '1412122225', 1);
+INSERT INTO EmployeesToProjects (emp_Id, proj_id) Values(1, 1);
